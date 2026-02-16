81afc8f8cbb7c8d6358c92e730c8bb32e7c05e40
Chima Kennedy Elorm <147996763+Mrole789@users.noreply.github.com>
2026-02-16 16:26:50 +0100

    Implement high score tracking in Scoreboard.
    Added high score functionality to the scoreboard.

--- scoreboard.py
+++ scoreboard.py
@@ -6,19 +6,36 @@ class Scoreboard(Turtle):
     """Creates scoreboard for keeping record of user progress"""
     def __init__(self):
         super().__init__()
+        
         self.score = 0
+        #Reads the previous highscore from the text file
+        with open("highscore.txt",mode="r") as file:
+            self.highscore = int(file.read())
+        
         self.color('white')
         self.penup()
         self.goto(0,275)
-        self.write(f'Score: {self.score}', False, align=ALIGN, font=FONT)
         self.hideturtle()
-        
+        self.write_score()
+     
+    def write_score(self):
+        self.clear()
+        self.write(f'Score: {self.score} High score: {self.highscore}', False, align=ALIGN, font=FONT)
+     
     def increase_score(self):
         self.score += 1
-        self.clear()
-        self.write(f'Score: {self.score}', False, align=ALIGN, font=FONT)
+        self.write_score()
 
-    def gameover(self):
-        self.goto(0,0)
-        self.color('red')
-        self.write('GAME OVER', False, align=ALIGN, font=FONT)
+    def s_reset(self):
+        if self.score > self.highscore:
+            self.highscore = self.score
+        
+        with open("highscore.txt",mode="w") as file:
+            file.write(str(self.highscore))
+        self.score = 0
+        self.write_score()
+    
+#     def gameover(self):
+#         self.goto(0,0)
+#         self.color('red')
+#         self.write('GAME OVER', False, align=ALIGN, font=FONT)
